754da3330f73d0fb55f647b7acef7023a2ff0d55
unknown <atyx300@naver.com>
2020-04-14 17:24:14 +0900

    first commit

---
+++ openmcp-apiserver/test.sh
@@ -0,0 +1,15 @@
+USERNAME="openmcp"
+PASSWORD="keti"
+IP=10.0.3.20
+PORT=31635
+URL="api"
+CLUSTER="cluster1"
+
+TOKEN_JSON=`curl -XGET -H "Content-type: application/json" "http://$IP:$PORT/token?username=$USERNAME&password=$PASSWORD"`
+TOKEN=`echo $TOKEN_JSON | jq .token`
+TOKEN=`echo "$TOKEN" | tr -d '"'`
+
+for ((;;)){
+	curl -XGET -H "Authorization: Bearer $TOKEN" -H "Content-type: application/json" $IP:$PORT/$URL?clustername=$CLUSTER
+	sleep 1
+}
