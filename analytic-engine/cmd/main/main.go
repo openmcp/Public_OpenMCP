2a8d0a6621afc710c9b300f115e267f68f9cdd8b
ChoiRiA <choiria@keti.re.kr>
2020-06-05 16:23:55 +0900

    openmcp-has-controller commit <analytic-engine> <openmcp-has-controller> <clientset>

---
+++ analytic-engine/cmd/main/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"openmcp-analytic-engine/pkg/analyticEngine"
+	//"openmcp-analytic-engine/pkg/protobuf"
+	"os"
+	"runtime"
+)
+
+const (
+	GRPC_PORT = "2050"
+)
+
+func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	INFLUX_IP := os.Getenv("INFLUX_IP")
+	INFLUX_PORT := os.Getenv("INFLUX_PORT")
+	INFLUX_USERNAME := os.Getenv("INFLUX_USERNAME")
+	INFLUX_PASSWORD := os.Getenv("INFLUX_PASSWORD")
+
+	//ae := analyticEngine.NewAnalyticEngine()
+	ae := analyticEngine.NewAnalyticEngine(INFLUX_IP, INFLUX_PORT, INFLUX_USERNAME, INFLUX_PASSWORD)
+
+	go ae.CalcResourceScore()
+
+	//a := protobuf.HASInfo{HPANamespace:"openmcp", HPAName:"openmcp-hpa", ClusterName:""}
+
+	//ae.SelectHPACluster(&a)
+	//mc.Influx.CreateDatabase()
+	//mc.Influx.CreateMeasurements()
+
+	ae.StartGRPC(GRPC_PORT)
+}
---
+++ analytic-engine/pkg/influx/influx.go
@@ -0,0 +1,55 @@
+package influx
+
+import (
+	"github.com/influxdata/influxdb/client/v2"
+	"log"
+	//"openmcp-analytic-engine/pkg/protobuf"
+)
+
+type Influx struct {
+	inClient client.Client
+}
+
+func NewInflux(INFLUX_IP, INFLUX_PORT, username, password string) *Influx {
+	inf := &Influx{
+		inClient: InfluxDBClient(INFLUX_IP, INFLUX_PORT, username, password),
+	}
+	return inf
+}
+func InfluxDBClient(INFLUX_IP, INFLUX_PORT, username, password string) client.Client {
+	c, err := client.NewHTTPClient(client.HTTPConfig{
+		Addr:     "http://" + INFLUX_IP + ":" + INFLUX_PORT,
+		Username: username,
+		Password: password,
+	})
+	if err != nil {
+		log.Fatalln("Error: ", err)
+	}
+	return c
+}
+
+func (in *Influx) GetClusterMetricsData(clusterName string) []client.Result {
+	q := client.NewQuery("SELECT * FROM Nodes WHERE cluster = '"+clusterName+"' GROUP BY * ORDER BY DESC LIMIT 2", "Metrics", "")
+
+	response, err := in.inClient.Query(q)
+
+	if err == nil && response.Error() == nil {
+		//fmt.Println(response.Results)
+		return response.Results
+	}
+
+	return nil
+
+}
+func (in *Influx) SelectMetricsData() []client.Result {
+	q := client.NewQuery("select * from Nodes group by * order by desc limit 1", "Metrics", "")
+
+	response, err := in.inClient.Query(q)
+
+	if err == nil && response.Error() == nil {
+		//fmt.Println(response.Results)
+		return response.Results
+	}
+
+	return nil
+}
