e831a510a375b60ed8063e953613422cf7eab162
chkim <atyx300@naver.com>
2020-06-12 10:06:17 +0900

    remove webstom v1.0 directory

--- resource-controller/controllers/openmcpingress/3.debug.sh
+++ resource-controller/controllers/openmcpingress/3.debug.sh
@@ -7,4 +7,4 @@ NAME=$(kubectl get pod -n $NS | grep -E $controller_name | awk '{print $1}')
 echo "Exec Into '"$NAME"'"
 
 #kubectl exec -it $NAME -n $NS /bin/sh
-kubectl logs -n $NS $NAME
+kubectl logs --follow -n $NS $NAME
