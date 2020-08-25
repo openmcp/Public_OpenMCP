822834835c9afffb9b9068d0021bfbd24773c186
chkim <atyx300@naver.com>
2020-08-25 14:50:54 +0900

    join/unjoin 시 log 출력, 시간 측정로그 출력

--- omcpctl/cmd/join.go
+++ omcpctl/cmd/join.go
@@ -19,11 +19,13 @@ import (
 	"fmt"
 	"github.com/spf13/cobra"
 	"k8s.io/client-go/tools/clientcmd"
+	"log"
 	"openmcp/openmcp/omcpctl/resource"
 	"openmcp/openmcp/util/clusterManager"
 	"path/filepath"
 	genericclient "sigs.k8s.io/kubefed/pkg/client/generic"
 	"strings"
+	"time"
 
 	//"k8s.io/client-go/tools/clientcmd"
 	cobrautil "openmcp/openmcp/omcpctl/util"
@@ -81,14 +83,14 @@ func moveToUnjoin(memberIP string) {
 
 	c := cobrautil.GetOmcpctlConf("/var/lib/omcpctl/config.yaml")
 
-	util.CmdExec("umount -l /mnt")
-	defer util.CmdExec("umount -l /mnt")
+	util.CmdExec2("umount -l /mnt")
+	defer util.CmdExec2("umount -l /mnt")
 
-	util.CmdExec("mount -t nfs " + c.NfsServer + ":/home/nfs/ /mnt")
+	util.CmdExec2("mount -t nfs " + c.NfsServer + ":/home/nfs/ /mnt")
 
 	openmcpIP := GetOutboundIP()
 
-	util.CmdExec("mv /mnt/openmcp/" + openmcpIP + "/members/join/" + memberIP + " /mnt/openmcp/" + openmcpIP + "/members/unjoin/" + memberIP)
+	util.CmdExec2("mv /mnt/openmcp/" + openmcpIP + "/members/join/" + memberIP + " /mnt/openmcp/" + openmcpIP + "/members/unjoin/" + memberIP)
 
 }
 
@@ -99,10 +101,10 @@ func getDiffJoinIP() []string {
 
 	c := cobrautil.GetOmcpctlConf("/var/lib/omcpctl/config.yaml")
 
-	util.CmdExec("umount -l /mnt")
-	defer util.CmdExec("umount -l /mnt")
+	util.CmdExec2("umount -l /mnt")
+	defer util.CmdExec2("umount -l /mnt")
 
-	util.CmdExec("mount -t nfs " + c.NfsServer + ":/home/nfs/ /mnt")
+	util.CmdExec2("mount -t nfs " + c.NfsServer + ":/home/nfs/ /mnt")
 	openmcpIP := GetOutboundIP()
 	nfsClusterJoinStr, err := util.CmdExec("ls /mnt/openmcp/" + openmcpIP + "/members/join")
 	nfsClusterJoinList := strings.Split(nfsClusterJoinStr, "\n")
@@ -134,12 +136,14 @@ func getDiffJoinIP() []string {
 }
 
 func joinCluster(memberIP string) {
+	start := time.Now()
+
 	c := cobrautil.GetOmcpctlConf("/var/lib/omcpctl/config.yaml")
 
 	util.CmdExec("umount -l /mnt")
 	defer util.CmdExec("umount -l /mnt")
 
-	util.CmdExec("mount -t nfs " + c.NfsServer + ":/home/nfs/ /mnt")
+	util.CmdExec2("mount -t nfs " + c.NfsServer + ":/home/nfs/ /mnt")
 
 	fmt.Println("Cluster Join Start")
 
@@ -170,12 +174,15 @@ func joinCluster(memberIP string) {
 	//cobrautil.WriteKubeConfig(kc, "/root/.kube/config_2")
 
 	cobrautil.WriteKubeConfig(kc, "/root/.kube/config")
-	util.CmdExec("mv /mnt/openmcp/" + openmcpIP + "/members/unjoin/" + memberIP + " /mnt/openmcp/" + openmcpIP + "/members/join/" + memberIP)
-	util.CmdExec("kubefedctl join " + cluster.Name + " --cluster-context " + cluster.Name + " --host-cluster-context openmcp --v=2")
+	util.CmdExec2("mv /mnt/openmcp/" + openmcpIP + "/members/unjoin/" + memberIP + " /mnt/openmcp/" + openmcpIP + "/members/join/" + memberIP)
+	util.CmdExec2("kubefedctl join " + cluster.Name + " --cluster-context " + cluster.Name + " --host-cluster-context openmcp --v=2")
 
 	installInitCluster(cluster.Name, c.OpenmcpDir)
 
 	fmt.Println("Cluster Join Completed - " + cluster.Name)
+
+	elapsed := time.Since(start)
+	log.Printf("Cluster Join Elapsed Time : %s", elapsed)
 }
 
 func installInitCluster(clusterName, openmcpDir string) {
@@ -183,14 +190,14 @@ func installInitCluster(clusterName, openmcpDir string) {
 	install_dir := filepath.Join(openmcpDir, "install_openmcp/member")
 	initYamls := []string{"custom-metrics-apiserver", "metallb", "metric-collector", "metrics-server", "nginx-ingress-controller"}
 
-	util.CmdExec("kubectl create ns openmcp --context " + clusterName)
+	util.CmdExec2("kubectl create ns openmcp --context " + clusterName)
 	for _, initYaml := range initYamls {
 		//fmt.Println("kubectl create -f " + install_dir + "/" + initYaml + " --context " + clusterName)
-		util.CmdExec("kubectl create -f " + install_dir + "/" + initYaml + " --context " + clusterName)
+		util.CmdExec2("kubectl create -f " + install_dir + "/" + initYaml + " --context " + clusterName)
 	}
 
-	util.CmdExec("chmod 755 " + install_dir + "/vertical-pod-autoscaler/hack/*")
-	util.CmdExec(install_dir + "/vertical-pod-autoscaler/hack/vpa-up.sh " + clusterName)
+	util.CmdExec2("chmod 755 " + install_dir + "/vertical-pod-autoscaler/hack/*")
+	util.CmdExec2(install_dir + "/vertical-pod-autoscaler/hack/vpa-up.sh " + clusterName)
 	fmt.Println("Init Module Deployment Finished - " + clusterName)
 }
 
