7602d087ee14be2d9d7815f4028e98f7a9fa5a0d
chkim <atyx300@naver.com>
2020-11-17 16:44:27 +0900

    Kch Commit
    [Namespace Controller]
    - 새로운 클러스터 Join시 Namespace리소스 자동 생성코드 추가
    
    [OpenMCP Sync Controller]
    - Reshape 감지 못하던 버그 수정
    
    [apis]
    - Namespace ChangeNeed Status 추가
    
    [clientset]
    - Namespace Clientset 추가

---
+++ clientset/v1alpha1/OpenMCPNamespace.go
@@ -0,0 +1,72 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/watch"
+	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/rest"
+	resourcev1alpha1 "openmcp/openmcp/apis/resource/v1alpha1"
+)
+
+type OpenMCPNamespaceInterface interface {
+	List(opts metav1.ListOptions) (*resourcev1alpha1.OpenMCPNamespaceList, error)
+	Get(name string, options metav1.GetOptions) (*resourcev1alpha1.OpenMCPNamespace, error)
+	Create(deployment *resourcev1alpha1.OpenMCPNamespace) (*resourcev1alpha1.OpenMCPNamespace, error)
+	Watch(opts metav1.ListOptions) (watch.Interface, error)
+	// ...
+}
+
+type OpenMCPNamespaceClient struct {
+	restClient rest.Interface
+	ns         string
+}
+
+func (c *OpenMCPNamespaceClient) List(opts metav1.ListOptions) (*resourcev1alpha1.OpenMCPNamespaceList, error) {
+	result := resourcev1alpha1.OpenMCPNamespaceList{}
+	err := c.restClient.
+		Get().
+		Namespace(c.ns).
+		Resource("openmcpnamespaces").
+		VersionedParams(&opts, scheme.ParameterCodec).
+		Do().
+		Into(&result)
+
+	return &result, err
+}
+
+func (c *OpenMCPNamespaceClient) Get(name string, opts metav1.GetOptions) (*resourcev1alpha1.OpenMCPNamespace, error) {
+	result := resourcev1alpha1.OpenMCPNamespace{}
+	err := c.restClient.
+		Get().
+		Namespace(c.ns).
+		Resource("openmcpnamespaces").
+		Name(name).
+		VersionedParams(&opts, scheme.ParameterCodec).
+		Do().
+		Into(&result)
+
+	return &result, err
+}
+
+func (c *OpenMCPNamespaceClient) Create(deployment *resourcev1alpha1.OpenMCPNamespace) (*resourcev1alpha1.OpenMCPNamespace, error) {
+	result := resourcev1alpha1.OpenMCPNamespace{}
+	err := c.restClient.
+		Post().
+		Namespace(c.ns).
+		Resource("openmcpnamespaces").
+		Body(deployment).
+		Do().
+		Into(&result)
+
+	return &result, err
+}
+
+func (c *OpenMCPNamespaceClient) Watch(opts metav1.ListOptions) (watch.Interface, error) {
+	opts.Watch = true
+	return c.restClient.
+		Get().
+		Namespace(c.ns).
+		Resource("openmcpnamespaces").
+		VersionedParams(&opts, scheme.ParameterCodec).
+		Watch()
+}
