a26c4da1e04c7065eecd38551dbd348f6a88848d
ChoiRiA <choiria@keti.re.kr>
2021-01-07 14:43:54 +0900

    docker private registry

--- openmcp-analytic-engine/1.build.sh
+++ openmcp-analytic-engine/1.build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+docker_registry_ip="10.0.3.40:5005"
 docker_id="openmcp"
-image_name="openmcp-analytic-engine"
+controller_name="openmcp-analytic-engine"
 
 export GO111MODULE=on
 go mod vendor
 
-go build -o build/_output/bin/$image_name -mod=vendor openmcp/openmcp/openmcp-analytic-engine/cmd/main && \
-docker build -t $docker_id/$image_name:v0.0.1 build && \
-docker push $docker_id/$image_name:v0.0.1
+go build -o build/_output/bin/$controller_name -mod=vendor openmcp/openmcp/openmcp-analytic-engine/cmd/main && \
+
+docker build -t $docker_registry_ip/$docker_id/$controller_name:v0.0.1 build && \
+docker push $docker_registry_ip/$docker_id/$controller_name:v0.0.1
--- openmcp-resource-controller/controllers/openmcp-has-controller/1.build.sh
+++ openmcp-resource-controller/controllers/openmcp-has-controller/1.build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+docker_registry_ip="10.0.3.40:5005"
 docker_id="openmcp"
 controller_name="openmcp-has-controller"
 
@@ -6,6 +7,6 @@ export GO111MODULE=on
 go mod vendor
 
 go build -o build/_output/bin/$controller_name -gcflags all=-trimpath=`pwd` -asmflags all=-trimpath=`pwd` -mod=vendor openmcp/openmcp/openmcp-resource-controller/controllers/$controller_name/pkg/main && \
-docker build -t $docker_id/$controller_name:v0.0.1 build && \
-docker push $docker_id/$controller_name:v0.0.1
 
+docker build -t $docker_registry_ip/$docker_id/$controller_name:v0.0.1 build && \
+docker push $docker_registry_ip/$docker_id/$controller_name:v0.0.1
