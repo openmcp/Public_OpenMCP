f081125c63545c2a8c86d87b49bde9ca057b83ad
jihoon <jihoon3150@gmailcom>
2021-06-07 17:56:23 +0900

    마이그레이션 샘플 데이터 갱신

--- openmcp-migration/1.build.sh
+++ openmcp-migration/1.build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 docker_id="openmcp"
-image_name="openmcp-migration"
+image_name="openmcp-snapshot"
 
 export GO111MODULE=on
 go mod vendor
 
-go build -o build/_output/bin/$image_name -gcflags all=-trimpath=`pwd` -asmflags all=-trimpath=`pwd` -mod=vendor openmcp/openmcp/openmcp-migration/cmd/manager && \
-docker build -t $docker_id/$image_name:v0.0.6 build && \
-docker push $docker_id/$image_name:v0.0.6
+go build -o build/_output/bin/$image_name -gcflags all=-trimpath=`pwd` -asmflags all=-trimpath=`pwd` -mod=vendor openmcp/openmcp/openmcp-snapshot/cmd/manager && \
+docker build -t $docker_id/$image_name:v1.0.2 build && \
+docker push $docker_id/$image_name:v1.0.2\
--- openmcp-migration/2.create.sh
+++ openmcp-migration/2.create.sh
@@ -1,2 +1,4 @@
 kubectl apply -f deploy/
-kubectl apply -f deploy/crds/openmcp.k8s.io_migrations_crd.yaml
+kubectl apply -f deploy/crds/
+#kubectl apply -f deploy/crds/openmcp.k8s.io_snapshotrestores_crd.yaml
+#kubectl apply -f deploy/crds/openmcp.k8s.io_snapshots_crd.yaml
