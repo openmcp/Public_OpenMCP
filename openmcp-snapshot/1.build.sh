77cc9c28b3554c82186a1934d63fffa479f0f28b
jihoon <jihoon3150@gmailcom>
2021-06-07 17:57:59 +0900

    스냅샷 샘플 데이터 갱신

--- openmcp-snapshot/1.build.sh
+++ openmcp-snapshot/1.build.sh
@@ -6,5 +6,5 @@ export GO111MODULE=on
 go mod vendor
 
 go build -o build/_output/bin/$image_name -gcflags all=-trimpath=`pwd` -asmflags all=-trimpath=`pwd` -mod=vendor openmcp/openmcp/openmcp-snapshot/cmd/manager && \
-docker build -t $docker_id/$image_name:v0.0.2 build && \
-docker push $docker_id/$image_name:v0.0.2\
+docker build -t $docker_id/$image_name:v1.0.2 build && \
+docker push $docker_id/$image_name:v1.0.2\
