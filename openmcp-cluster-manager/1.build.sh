f895d1ffaeea903bf1534ab5de3145ba46170879
chkim <atyx300@naver.com>
2021-07-23 18:00:58 +0900

    update

--- openmcp-cluster-manager/1.build.sh
+++ openmcp-cluster-manager/1.build.sh
@@ -7,5 +7,5 @@ go mod vendor
 
 go build -o build/_output/bin/$controller_name -gcflags all=-trimpath=`pwd` -asmflags all=-trimpath=`pwd` -mod=vendor openmcp/openmcp/$controller_name/src/main && \
 
-docker build -t $docker_id/$controller_name:v0.0.2 build && \
-docker push $docker_id/$controller_name:v0.0.2
+docker build -t $docker_id/$controller_name:v0.0.1 build && \
+docker push $docker_id/$controller_name:v0.0.1
