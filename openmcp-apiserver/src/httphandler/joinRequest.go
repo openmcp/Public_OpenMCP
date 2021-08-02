45146dc043d4cdd2327343e6eb1d5ad8c737e5eb
ChoiRiA <choiria@keti.re.kr>
2021-08-02 16:24:01 +0900

    UPDATE

--- openmcp-apiserver/src/httphandler/joinRequest.go
+++ openmcp-apiserver/src/httphandler/joinRequest.go
@@ -8,6 +8,7 @@ import (
 	clusterv1alpha1 "openmcp/openmcp/apis/cluster/v1alpha1"
 	cobrautil "openmcp/openmcp/omcpctl/util"
 	"openmcp/openmcp/omcplog"
+	"strings"
 
 	"admiralty.io/multicluster-controller/pkg/cluster"
 	"gopkg.in/yaml.v2"
@@ -17,6 +18,7 @@ import (
 var Live *cluster.Cluster
 
 func JoinHandler(w http.ResponseWriter, r *http.Request) {
+	nodeinfo := r.FormValue("data")
 
 	file, _, err := r.FormFile("file")
 
@@ -32,7 +34,7 @@ func JoinHandler(w http.ResponseWriter, r *http.Request) {
 	c := &cobrautil.KubeConfig{}
 	err = yaml.Unmarshal(fileBytes, c)
 
-	CreateClusterResource(c.Clusters[0].Name, fileBytes)
+	CreateClusterResource(c.Clusters[0].Name, fileBytes, nodeinfo)
 
 	a := []byte("OK\n")
 	w.Write(a)
@@ -45,7 +47,7 @@ func JoinCloudHandler(w http.ResponseWriter, r *http.Request) {
 
 	c_name := r.Form.Get("clustername")
 	c_type := r.Form.Get("clustertype")
-	access_token := r.Form.Get("accesstoken")
+	//access_token := r.Form.Get("accesstoken")
 
 	fmt.Println(c_name, " / ", c_type)
 
@@ -64,13 +66,52 @@ func JoinCloudHandler(w http.ResponseWriter, r *http.Request) {
 	c := &cobrautil.KubeConfig{}
 	err = yaml.Unmarshal(fileBytes, c)
 
-	CreateCloudClusterResource(c_name, c_type, access_token, fileBytes)
+	CreateCloudClusterResource(c_name, c_type, fileBytes)
 
 	a := []byte("OK\n")
 	w.Write(a)
 }
 
-func CreateClusterResource(name string, config []byte) (string, error) {
+func CreateClusterResource(name string, config []byte, nodeinfo string) (string, error) {
+	regionmap := map[string]string{}
+	zonemap := map[string]string{}
+
+	slice1 := strings.Split(nodeinfo, "#")
+
+	for _, str := range slice1 {
+		if len(str) > 0 {
+			slice2 := strings.Split(str, ",")
+
+			if len(slice2) == 3 {
+				regionmap[slice2[1]] = ""
+				zonemap[slice2[2]] = ""
+			}
+		}
+	}
+
+	regionstr := ""
+	zonestr := ""
+
+	for key, _ := range regionmap {
+		if regionstr != "" {
+			regionstr = regionstr + "," + key
+		}
+		regionstr = key
+	}
+
+	for key, _ := range zonemap {
+		if zonestr != "" {
+			zonestr = zonestr + "," + key
+		}
+		zonestr = key
+	}
+
+	ni := clusterv1alpha1.NodeInfo{
+		Region: regionstr,
+		Zone:   zonestr,
+	}
+
+	omcplog.V(4).Info("'", name, "' nodeInfo ", ni.Region, " / ", ni.Zone)
 
 	clusterCR := &clusterv1alpha1.OpenMCPCluster{
 		TypeMeta: v1.TypeMeta{
@@ -84,6 +125,7 @@ func CreateClusterResource(name string, config []byte) (string, error) {
 		Spec: clusterv1alpha1.OpenMCPClusterSpec{
 			ClusterPlatformType: "On-Premise",
 			JoinStatus:          "UNJOIN",
+			NodeInfo:            ni,
 			MetalLBRange: clusterv1alpha1.MetalLBRange{
 				AddressFrom: "",
 				AddressTo:   "",
@@ -97,16 +139,16 @@ func CreateClusterResource(name string, config []byte) (string, error) {
 	err := liveClient.Create(context.TODO(), clusterCR)
 
 	if err != nil {
-		omcplog.V(4).Info("Fail to create openmcpcluster resource")
+		omcplog.V(4).Info("Fail to create openmcpcluster resource'", name, "'")
 		fmt.Println(err)
 	} else {
-		omcplog.V(4).Info("Success to create openmcpcluster resource")
+		omcplog.V(4).Info("Success to create openmcpcluster resource '", name, "'")
 	}
 
 	return clusterCR.Name, err
 }
 
-func CreateCloudClusterResource(c_name string, c_type string, access_token string, config []byte) (string, error) {
+func CreateCloudClusterResource(c_name string, c_type string, config []byte) (string, error) {
 
 	clusterCR := &clusterv1alpha1.OpenMCPCluster{
 		TypeMeta: v1.TypeMeta{
