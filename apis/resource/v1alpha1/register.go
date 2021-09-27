a39abe600e27549eaf21cdc23c401324af3f1588
ChoiRiA <choiria@keti.re.kr>
2021-09-27 14:27:14 +0900

    UPDATE

--- apis/resource/v1alpha1/register.go
+++ apis/resource/v1alpha1/register.go
@@ -50,6 +50,8 @@ func addKnownTypes(scheme *runtime.Scheme) error {
 		&OpenMCPNamespaceList{},
 		&OpenMCPVirtualService{},
 		&OpenMCPVirtualServiceList{},
+		&OpenMCPPersistentVolumeClaimList{},
+		&OpenMCPPersistentVolumeList{},
 	)
 	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
 	return nil
