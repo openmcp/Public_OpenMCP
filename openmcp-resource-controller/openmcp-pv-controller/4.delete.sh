b605b1f6f0c223aef2366945e98f1ce919fbe635
ChoiRiA <choiria@keti.re.kr>
2021-11-15 14:49:53 +0900

    PV,PVC UPDATE

--- openmcp-resource-controller/openmcp-pv-controller/4.delete.sh
+++ openmcp-resource-controller/openmcp-pv-controller/4.delete.sh
@@ -2,6 +2,6 @@
 
 cd deploy/
 
-kubectl delete -f service_account.yaml
-kubectl delete -f role_binding.yaml
+#kubectl delete -f service_account.yaml
+#kubectl delete -f role_binding.yaml
 kubectl delete -f operator.yaml
