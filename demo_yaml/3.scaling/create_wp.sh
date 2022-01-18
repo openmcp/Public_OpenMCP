de24d46e3d3194afea8b5859f979073a4b334f23
ChoiRiA <choiria@keti.re.kr>
2022-01-18 14:11:56 +0900

    Scaling Demo Yaml UPDATE

--- demo_yaml/3.scaling/create_wp.sh
+++ demo_yaml/3.scaling/create_wp.sh
@@ -1,4 +1,5 @@
 #mkdir /home/nfs/wordpress/data001/pv001
 #mkdir /home/nfs/wordpress/data001/pv002
-kubectl apply -f 2.deploy/.
-kubectl apply -f 4.ingress/.
+kubectl apply -f 2.deploy/policy/policy.yaml
+kubectl create -f 2.deploy/.
+kubectl create -f 4.ingress/.
